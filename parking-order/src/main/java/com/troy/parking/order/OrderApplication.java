6de65200fe00714c6ac98136eafa26642b02e064
caoqiang <caoqiang@77pay.com.cn>
2020-04-29 18:03:13 +0800

    common

--- parking-order/src/main/java/com/troy/parking/order/OrderApplication.java
+++ parking-order/src/main/java/com/troy/parking/order/OrderApplication.java
@@ -1,5 +1,6 @@
 package com.troy.parking.order;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Version 1.0
  */
 @SpringBootApplication
+@MapperScan("com.troy.parking.order.dao")
 public class OrderApplication {
 
     public static void main(String[] args) {
